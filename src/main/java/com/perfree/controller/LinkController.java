ae0e76c1ecda9e5905d6501233b8aed2a15173c8
Perfree <perfree@126.com>
2019-03-26 20:01:33 +0800

    改吧改吧

--- src/main/java/com/perfree/controller/LinkController.java
+++ src/main/java/com/perfree/controller/LinkController.java
@@ -2,7 +2,6 @@ package com.perfree.controller;
 
 import com.jfinal.aop.Inject;
 import com.jfinal.core.ActionKey;
-import com.perfree.model.LinkModel;
 import com.perfree.service.LinkService;
 import io.jboot.web.controller.annotation.RequestMapping;
 import io.jpress.core.menu.annotation.AdminMenu;
@@ -19,11 +18,14 @@ public class LinkController extends AdminControllerBase {
     private LinkService linkService;
 
     @ActionKey("/admin/perfreeLink")
-    @AdminMenu(groupId = "perfreeLink", text = "友链管理")
+    @AdminMenu(groupId = "perfreeLink", text = "友链管理",order = 1)
     public void index() {
-        LinkModel linkModel = new LinkModel();
-        linkModel.set("name","测试").set("link","测试").set("desc","测试").set("imgUrl","测试");
-        linkService.addLink(linkModel);
         render("perfreeLink/index.html");
+    }
+
+    @ActionKey("/admin/perfreeLink/add")
+    @AdminMenu(groupId = "perfreeLink", text = "新建",order = 2)
+    public void add() {
+        render("perfreeLink/add.html");
     }
 }
